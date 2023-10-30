a80effa020b8c36ccc1cae45756beb1767362ff3
Sven Lange <sven@findify.io>
2023-10-30 16:03:47 +0100

    Resilient bootstrap when no custom plugins exist

--- charts/rundeck/scripts/bootstrap.sh
+++ charts/rundeck/scripts/bootstrap.sh
@@ -17,8 +17,12 @@ if test -f "$FRAMEWORK_SRC"; then
     cp "$FRAMEWORK_SRC" /tmp/remco-partials/framework/framework-custom.properties
 fi
 
-echo "Copying custom plugins"
-cp -r /mnt/plugins/. /home/rundeck/libext
+if test -d /mnt/plugins; then
+    echo "Copying custom plugins"
+    cp -r /mnt/plugins/. /home/rundeck/libext
+else
+    echo "Starting without custom plugins because mount point '/mnt/plugins' does not exist"
+fi
 
 echo "Continue with common bootstrap"
 exec /home/rundeck/docker-lib/entry.sh
